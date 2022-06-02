1e3dc73e78455001cf6c314f0d59ed548f119a6a
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-02 13:55:30 +0600

    first commit

---
+++ nasted-condition.js
@@ -0,0 +1,164 @@
+//  Advance nasted condition..
+// bassically nasted mane hocche ektar pichone e ekta ache emon kichu...
+//  na hole onno ekta...mone koren ami dokan e gelam...then amar kache amar bujet er modhe kivhu khabar ami nibo ,,,ami jodi danish kinte cai,,are setar tk jodi na thake,,,tahole ami else if use kore onno ekta product caibo dokan dar er theke ....jemon mama danish na thakle amake batarbread den emon kichu hoiole amra (else if use korbo )...
+
+
+// nested condition part-1
+
+
+var danishprice = 420;
+
+var batarbreadprice = 430;
+
+var korola = 440;
+
+var jamrul = 450;
+
+var misty = 460;
+
+var licu = 470;
+
+var amm = 80;
+
+var kathal = 90;
+
+var dalim = 100;
+
+var frutika = 120;
+
+var mama = 130;
+
+var haluwa = 140;
+
+var zalmuri = 150;
+
+var cicingga = 160;
+
+var lauu = 170;
+
+var mybujet = 150;
+
+
+if (danishprice < mybujet) {
+
+    console.log('danish kheye khusite lafalafi korbo ');
+}
+
+
+
+
+else if (batarbreadprice < mybujet) {
+
+    console.log('batar bread diye khabar khamu ');
+
+}
+
+
+else if (korola < mybujet) {
+    console.log('korola diye khamu ')
+}
+
+
+
+
+else if (jamrul < mybujet) {
+
+    console.log('jamruldiye khabar khamu ');
+
+}
+
+
+
+
+
+else if (misty < mybujet) {
+
+    console.log('misty diye khabar khamu ');
+
+}
+
+
+
+
+else if (licu < mybujet) {
+
+    console.log('licu diye khabar khamu ');
+
+}
+
+
+else if (amm < mybujet) {
+
+    console.log('amm diye khabar khamu ');
+
+}
+
+
+
+
+else if (kathal < mybujet) {
+
+    console.log('kathal diye khabar khamu ');
+
+}
+
+
+
+else if (dalim < mybujet) {
+
+    console.log('batar bread diye khabar khamu ');
+
+}
+
+
+
+
+else if (frutika < mybujet) {
+
+    console.log('frutika diye khabar khamu ');
+
+}
+
+
+
+else if (mama < mybujet) {
+
+    console.log('mama diye khabar khamu ');
+
+}
+
+
+
+else if (haluwa < mybujet) {
+
+    console.log('haluwa diye khabar khamu ');
+
+}
+
+
+
+else if (zalmuri < mybujet) {
+
+    console.log('zalmuri diye khabar khamu ');
+
+}
+
+else if (cicingga < mybujet) {
+
+    console.log(' cicingga diye khabar khamu ');
+
+}
+
+
+
+else if (lauu < mybujet) {
+
+    console.log('lauu  diye khabar khamu ');
+
+}
+
+
+else {
+
+    console.log('ki are korarar ami kichu khabo na ,sudhu batash khamu are ekta darby biri foraya tention dur kormu ');
+}
---
+++ nasting-condition.js
@@ -0,0 +1,25 @@
+// amra eikhane if else er vitore arekta if else likhte parbo...sorter vitore sorto thakbe eikhane 
+
+
+var danishprice = 420;
+var packedwell = false;
+var mybujet = 100;
+
+
+
+
+if (danishprice < mybujet) {
+
+
+    if (packedwell == true) {
+        console.log('danish khabo')
+    }
+    else {
+
+        console.log('danish khamu na ')
+    }
+}
+else {
+
+    console.log('danish kamu kemne jodi tk ei na thake,,,bad ')
+}
